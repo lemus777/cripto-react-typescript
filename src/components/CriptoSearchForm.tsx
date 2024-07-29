f9b56854a1aaeb3a3e1696bbfb19d6c6daca1567
lemus <lemus777@gmail.com>
2024-07-29 23:05:21 +0200

    getting the currency and crypto to be valued

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -6,6 +6,8 @@ import ErrorMessage from "./ErrorMessage"
 
 export default function CriptoSearchForm() {
   const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+  const fetchData = useCryptoStore((state) => state.fetchData)
+
   const [pair, setPair] = useState<Pair>({
     currency: '',
     cryptocurrency: ''
@@ -27,7 +29,7 @@ export default function CriptoSearchForm() {
     }
     setError('')
 
-    // Consultar la API
+    fetchData(pair)
   }
 
   return (
