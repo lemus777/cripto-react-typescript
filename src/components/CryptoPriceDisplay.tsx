26176f5a2441f4d28f25cc3dce43f11167f537aa
lemus <lemus777@gmail.com>
2024-07-30 23:10:42 +0200

    CSS for result component

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -6,14 +6,21 @@ export default function CryptoPriceDisplay() {
   const hasResult = useMemo(() => !Object.values(result).includes('') , [result])
 
   return (
-    <div>
+    <div className="result-wrapper">
       {hasResult && (
         <>
           <h2>Cotización</h2>
           <div className="result">
-
+            <img
+              src={`https://cryptocompare.com/${result.IMAGEURL}`}
+              alt="Imagen Cryptomoneda"
+            />
             <div>
               <p>El precio es de: <span>{result.PRICE}</span></p>
+              <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+              <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+              <p>Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+              <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
             </div>
           </div>
         </>
