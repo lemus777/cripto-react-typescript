47f669fcb9dbfed53b85f0176cadd63737e6f6a2
lemus <lemus777@gmail.com>
2024-07-30 16:58:47 +0200

    calling the API to get the current crypto value

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -12,5 +12,7 @@ export async function getCryptos() {
 }
 
 export async function fetchCurrentCryptoPrice(pair: Pair) {
-  console.log(pair)
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptocurrency}&tsyms=${pair.currency}`
+  const { data: {DISPLAY} } = await axios(url)
+  console.log(DISPLAY[pair.cryptocurrency][pair.currency])
 }
